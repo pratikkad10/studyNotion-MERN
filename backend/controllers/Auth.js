ff4155598bcac5650a27e0fda3007be1dd99f9dd
pratik kad <pratikkad22@gmail.com>
2025-03-17 19:20:12 +0530

    OTP generate function completed

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,6 +1,58 @@
+const User=require('../models/User');
+const OTP=require('../models/OTP');
+const otpGenerator=require('otp-generator');
+
 
 //sendOTP
+exports.sendOTP = async (req,res)=>{
+    try {
+        const {email}=req.body;
+        const checkUserExists=await User.findOne({email});
+
+        if(checkUserExists){
+            return res.status(401).json({
+                success:false,
+                message:"User already exixts!"
+            })
+        }
+
+        var otp = otpGenerator.generate(6, {
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false
+        });
+
+        let result= OTP.findOne({otp: otp});
+
+        if(result){
+            otp = otpGenerator.generate(6, {
+                upperCaseAlphabets:false,
+                lowerCaseAlphabets:false,
+                specialChars:false
+            });
+        }
+
+        const otpPayload={email, otp};
+
+        const otpBody=await OTP.create(otpPayload);
+
+        res.status(200).json({
+            success:true,
+            message:"otp sent successfully!",
+            otp:otp
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message:"Internal server error",
+            error:error.message
+        })
+    }
+} 
 
 //signup
 
 //login
+
+//change password
