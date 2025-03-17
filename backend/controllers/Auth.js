e84e118442707a0398d9ca2336ba4232a5a81ff8
pratik kad <pratikkad22@gmail.com>
2025-03-17 19:53:51 +0530

    signup controller completed

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,7 +1,8 @@
 const User=require('../models/User');
 const OTP=require('../models/OTP');
 const otpGenerator=require('otp-generator');
-
+const bcrypt=require('bcrypt');
+const Profile = require('../models/Profile');
 
 //sendOTP
 exports.sendOTP = async (req,res)=>{
@@ -52,6 +53,88 @@ exports.sendOTP = async (req,res)=>{
 } 
 
 //signup
+exports.signup = async (req,res)=>{
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp
+        } = req.body;
+
+        if(!firstName|| !lastName || !email || !password || !confirmPassword || !accountType || !contactNumber || !otp){
+            return res.status(403).json({
+                success:false,
+                message:"All fields are required",
+            })
+        }
+
+        if(password !== confirmPassword){
+            return res.status(400).json({
+                success:false,
+                message:"password not matched!",
+            })
+        }
+
+        const existingUser= await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"User already exists!",
+            })
+        }
+
+        const recentOTP= await OTP.find({email}).sort({createdAt:-1}).limit(1);
+        if(recentOTP.length == 0){
+            return res.status(400).json({
+                success:false,
+                message:"OTP not found!",
+            })
+        }
+        else if(otp !== recentOTP){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid OTP!",
+            }) 
+        }
+
+        const hashedPassword= await bcrypt.hash(password, 10);
+
+        const profileDetails=await Profile.create({
+            gender:null,
+            dateOfBirth:null,
+            about:null,
+            contactNumber
+        })
+
+        const newUser= await User.create({firstName,
+            lastName,
+            email,
+            accountType,
+            password:hashedPassword,
+            additionalDetails:profileDetails._id,
+            image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName}%20${lastName}`
+        })
+
+        res.status(200).json({
+            success:true,
+            message:"User created successfully!",
+            user:newUser
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"User cannot Registered!",
+            error:error.message
+        })
+    }
+}
 
 //login
 
